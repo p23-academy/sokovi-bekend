8dac0349c29649528b6c01cab5ad0faf8eb2e74e
Amči <a@p23.io>
2024-07-26 16:02:26 +0200

    postgresql init + registration

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 const cors = require('cors');
+const {registerUser} = require("./users");
+const {initializePostgres} = require("./db");
 
 app.use(cors());
 app.use(express.json());
@@ -12,4 +14,25 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
+})
+
+app.post('/init', async (req, res) => {
+  try {
+    await initializePostgres()
+    return res.status(200).json({})
+  } catch (error) {
+    console.log(error)
+    return res.status(400).json(error)
+  }
+});
+
+app.post('/register', async (req, res) => {
+  try {
+    const {email, password} = req.body
+    const user = await registerUser(email, password)
+    return res.status(200).json(user)
+  } catch (error) {
+    console.log(error)
+    return res.status(400).json(error)
+  }
 })
--- db.js
+++ db.js
@@ -1,6 +1,26 @@
 // db.js
-import postgres from 'postgres'
+const postgres = require('postgres')
 
-const sql = postgres({ /* options */ }) // will use psql environment variables
+const sql = postgres({
+  host: 'localhost',            // Postgres ip address[s] or domain name[s]
+  port: 5432,          // Postgres server port[s]
+  database: 'sokovi',            // Name of database to connect to
+  username: 'postgres',            // Username of database user
+  password: 'postgres',            // Password of database user
+})
 
-export default sql
+const initializePostgres = async () => {
+  await sql`
+    DROP TABLE IF EXISTS users
+  `
+  await sql`
+    CREATE TABLE users (
+      id SERIAL PRIMARY KEY,
+      email varchar(100) NOT NULL UNIQUE,
+      password varchar(450) NOT NULL,
+      role varchar(20) NOT NULL
+    )
+   `
+}
+
+module.exports = {sql, initializePostgres}
